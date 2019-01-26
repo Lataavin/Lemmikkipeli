832f07f8897868521c0b99c1eb065eb999b1760d
CudlPudl <34170840+CudlPudl@users.noreply.github.com>
2019-01-26 22:53:10 +0200

    Eläimet flippaa kun suunta muuttuu, bugi korjattu jossa eläin flippaa loputtomasti maailman laidalla.

--- Assets/Scripts/CreatureBehaviour.cs
+++ Assets/Scripts/CreatureBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CreatureBehaviour : MonoBehaviour
 {
     public float movementSpeed = 10f;
+    [SerializeField]
+    private SpriteRenderer _renderer;
     private float mSpeed = 0;
     public float MSpeed { get { return mSpeed; } set { mSpeed = 360 / value; } }
     private float rotation = 0;
@@ -16,6 +18,7 @@ public class CreatureBehaviour : MonoBehaviour
 
         moveDir = Random.Range(0, 2);
         if (moveDir == 0) { moveDir = -1; }
+        _renderer.flipX = moveDir >= 0;
         rotation = Random.Range(0, WorldManager.instance.worldSize);
         transform.localPosition = new Vector3(rotation, transform.localEulerAngles.y, transform.localEulerAngles.z);
     }
@@ -33,7 +36,12 @@ public class CreatureBehaviour : MonoBehaviour
     public void Move(float amount)
     {
         rotation += amount;
-        if (rotation <= 0 || rotation >= WorldManager.instance.worldSize) { moveDir *= -1; }
+        if (rotation < 0 || rotation > WorldManager.instance.worldSize)
+        {
+            moveDir *= -1;
+            _renderer.flipX = moveDir >= 0;
+            Mathf.Clamp(rotation, 0, WorldManager.instance.worldSize);
+        }
         transform.localPosition = new Vector3(rotation, transform.localPosition.y, transform.localPosition.z);
     }
 }
