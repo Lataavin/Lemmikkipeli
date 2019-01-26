654a58503cda3eb2d08900d7298ff2b22ce203d2
CudlPudl <34170840+CudlPudl@users.noreply.github.com>
2019-01-26 21:05:00 +0200

    StartUpScene, Credits screen

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private string _sceneName;
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+}
