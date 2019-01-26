cda8292a498b4f84bfcb60424de4e7414df8212d
CudlPudl <34170840+CudlPudl@users.noreply.github.com>
2019-01-26 17:35:36 +0200

    Efektejä

---
+++ Assets/Scripts/DestroyMyself.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMyself : MonoBehaviour
+{
+    [SerializeField]
+    private float _duration = 1f;
+    private float _timer = 0f;
+
+    public void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _duration)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
