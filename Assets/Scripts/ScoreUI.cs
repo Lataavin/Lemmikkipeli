1b66349fa12e5982272807c16301ea1a34fa434d
Mikko Niiranen <mikkojniiranen@gmail.com>
2019-01-27 11:04:09 +0200

    Score ui prefab

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    TMPro.TextMeshProUGUI _scoreLabel;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _scoreLabel.text = string.Format("Score: {0}, Combo: {1}", WorldManager.instance.GameScore, WorldManager.instance.ExtraScore);
+    }
+}
