2801bc3fa7a74a36f8b12885b0dcbe199959b7f6
CudlPudl <34170840+CudlPudl@users.noreply.github.com>
2019-01-27 13:48:32 +0200

    FeverMode

---
+++ Assets/Scripts/FeverMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeverMode : MonoBehaviour
+{
+    [SerializeField]
+    private Image _image;
+
+    public void SetValue(float value)
+    {
+        _image.CrossFadeAlpha(value, 0.5f, true);
+    }
+}
